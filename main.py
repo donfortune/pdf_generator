30c95895e9c3107ab3377596560afe993dc29b24
donfortune <donfortunet.df@gmail.com>
2023-04-10 10:38:41 +0100

    added more pages dynamically

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ pdf = FPDF(orientation='P', unit='mm', format='A4')
 data = pd.read_csv('topics.csv')
 
 for index, rows in data.iterrows():
-
     pdf.add_page()
+    for i in range(rows['Pages']):
+        pdf.add_page()
     pdf.set_text_color(100, 100, 100)
     pdf.set_font(family='Times', style='B', size=12)
     pdf.cell(w=0, h=12, txt=rows['Topic'], align='L', ln=1, border=0)
     pdf.line(10, 20, 200, 20)
+    #pdf.footer(rows['Topic'])
 
 
 pdf.output('output.pdf')
